01497e4ba4c7266ded7fc92196d6000e55778024
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-03-03 23:33:12 -0300

    fix: add missing config to @types folder

--- @types/module.d.ts
+++ @types/module.d.ts
@@ -4,6 +4,8 @@ declare module '@x-spacy/kafka' {
     port: number;
     username: string;
     password: string;
+    securityProtocol: 'plaintext' | 'ssl' | 'sasl_plaintext' | 'sasl_ssl' | undefined;
+    mechanism: string;
     groupId: string;
   }
   export class KafkaModule {
